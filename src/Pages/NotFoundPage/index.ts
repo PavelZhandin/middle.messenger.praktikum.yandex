7e60ff1a7c51608d55e3546c7e5966317ea0d8a9
Zhandin Pavel <pavelzhandin@gmail.com>
2024-07-23 22:56:07 +0300

    новая логика компонента страницы 404

--- src/Pages/NotFoundPage/index.ts
+++ src/Pages/NotFoundPage/index.ts
@@ -1,3 +1,15 @@
+import Block from "../../Core/Block";
 import "./index.scss";
 
-export { default as NotFoundPage } from "./template.hbs?raw";
+// export { default as NotFoundPage } from "./template.hbs?raw";
+import pageTemplate from "./template.hbs?raw";
+
+export class NotFoundPage extends Block {
+  constructor() {
+    super({ events: {} });
+  }
+
+  protected render(): string {
+    return pageTemplate;
+  }
+}
