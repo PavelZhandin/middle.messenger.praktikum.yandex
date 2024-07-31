f491c3e597bd9d9bfa639ea5f6998311acb74148
Zhandin Pavel <pavelzhandin@gmail.com>
2024-07-31 11:19:10 +0300

    фикс ошибок линтеров

--- src/Utils/compareFunctions/isDeepEqual.ts
+++ src/Utils/compareFunctions/isDeepEqual.ts
@@ -1,4 +1,5 @@
 import { isEqual } from "./isEqual";
+import { isObject } from "./isObject";
 
 export function isDeepEqual(
     object1: Record<string, unknown>,
@@ -13,6 +14,21 @@ export function isDeepEqual(
 
     // eslint-disable-next-line no-restricted-syntax
     for (const key of keys1) {
+        const value1 = object1[key];
+        const value2 = object2[key];
+        const bothAreObjects = isObject(value1) && isObject(value2);
+
+        if (
+            (bothAreObjects &&
+                !isDeepEqual(
+                    value1 as Record<string, unknown>,
+                    value2 as Record<string, unknown>,
+                )) ||
+            (!bothAreObjects && value1 !== value2)
+        ) {
+            return false;
+        }
+
         if (!isEqual(object1[key], object2[key])) {
             return false;
         }
